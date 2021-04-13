75fbadf0692e391fbed62644b8abdc6d90f8a88a
Mikarific <mikerific2@gmail.com>
2021-04-13 01:56:33 -0600

    Change init.js to add command aliases.

--- init.js
+++ init.js
@@ -4,16 +4,29 @@ const fs = require('fs');
 const axios = require('axios');
 
 const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
+const commands = [];
 
-function addCommand(file, index) {
+for (const file of commandFiles) {
 	const command = require(`./src/commands/${file}`);
 	if(command.options) {
-		const commandJSON = {
+		commands.push({
 			'name': command.name,
 			'description': command.description,
 			'options': command.options,
-		};
-		axios.post(`https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`, commandJSON, {
+		});
+		for (const alias of command.aliases) {
+			commands.push({
+				'name': alias,
+				'description': command.description,
+				'options': command.options,
+			});
+		}
+	}
+}
+
+function addCommand(command, index) {
+	if(command.options) {
+		axios.post(`https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`, command, {
 			headers: { Authorization: `Bot ${process.env.BOT_TOKEN}` },
 		}).then(function(response) {
 			if(response.status === 200 || response.status === 201) {
@@ -22,16 +35,16 @@ function addCommand(file, index) {
 				console.log(`Couldn't add command ${command.name}. Response code ${response.status}.`);
 			}
 			setTimeout(function() {
-				if(index + 1 < commandFiles.length) {
-					addCommand(commandFiles[index + 1], index + 1);
+				if(index + 1 < commands.length) {
+					addCommand(commands[index + 1], index + 1);
 				}
 			}, 5000);
 		}).catch(function() {
 			console.log(`Couldn't add command ${command.name}. Error.`);
 		});
-	} else if(index + 1 < commandFiles.length) {
-		addCommand(commandFiles[index + 1], index + 1);
+	} else if(index + 1 < commands.length) {
+		addCommand(commands[index + 1], index + 1);
 	}
 }
 
-addCommand(commandFiles[0], 0);
+addCommand(commands[0], 0);
